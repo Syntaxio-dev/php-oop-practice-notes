b4af9d43d8c7565f0bc50f8dd69e11e2a0ceca22
Ayush <ayushicon1@gmail.com>
2026-06-20 16:53:58 +0530

    some adjustment

--- form_handling_oop.php
+++ form_handling_oop.php
@@ -1,10 +1,23 @@
 <?php
+if (isset($_POST['user'])){
+    class user{
+        function getname($name){
+            echo "User name is $name";
+        }
+    }
 
+    $user= new user();
+    $user->getname($_POST['user']);
+}
 
 
 ?>
 
 <form action="" method="post">
-    <input type="text" placeholder="Enter your name ">
+    <input type="text" name="user" placeholder="Enter your name ">
+    <br>
+    <br>
+    <br>
+    <button>clinc it </button>
 
 </form>
